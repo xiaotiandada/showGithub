91eb37ab5984839fe45a57caa5a8b3d33b1f1ee8
_xiaotian <952822399@qq.com>
2019-01-03 17:46:47 +0800

    add header
    add header

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import 'antd/dist/antd.css'
+import './index.css'
 
 import App from './page/index/index';
 import * as serviceWorker from './serviceWorker';
--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,15 +1,41 @@
 import React, { Component } from "react";
 import "./index.css";
-import { DatePicker } from "antd";
-import { Button } from "antd";
+import { Input } from "antd";
+import { Menu, Icon } from "antd";
+import { Avatar } from "antd";
+
+const Search = Input.Search;
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 
 class App extends Component {
+  state = {
+    current: "mail"
+  };
+
+  handleClick = e => {
+    console.log("click ", e);
+    this.setState({
+      current: e.key
+    });
+  };
+
   render() {
     return (
-      <div>
-        <DatePicker />
-        <Button type="primary"> Primary </Button>
-      </div>
+      <header className="search-name header">
+        <Search
+          style={{
+            display: "none"
+          }}
+          size="large"
+          placeholder="请输入Githun用户名"
+          onSearch={value => console.log(value)}
+          enterButton
+        />
+        <div className="avatar-img">
+          <Avatar size="large" icon="user" />
+        </div>
+      </header>
     );
   }
 }
