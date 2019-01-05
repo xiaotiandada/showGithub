badadc42143f844ec05e9de7e1c19f8ee8f5f588
_xiaotian <952822399@qq.com>
2019-01-05 14:22:19 +0800

    header
    header

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -1,7 +1,6 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import "./index.css";
-import { Input } from "antd";
-import { Avatar } from "antd";
+import { Input, Avatar, Alert } from "antd";
 
 const Search = Input.Search;
 
@@ -9,10 +8,17 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
+      userName: "",
       headerStatus: false,
-      current: "mail"
+      current: "mail",
+      message: "",
+      type: "",
+      inShow: false
     };
     this.toggleHeader = this.toggleHeader.bind(this);
+    this.changeSearchValue = this.changeSearchValue.bind(this);
+    this.toggleAlertShow = this.toggleAlertShow.bind(this);
+    this.searchValue = this.searchValue.bind(this);
   }
 
   toggleHeader() {
@@ -21,27 +27,90 @@ class App extends Component {
     });
   }
 
+  changeSearchValue(e) {
+    let val = e.target.value;
+    this.setState({
+      userName: val
+    });
+  }
+
+  searchValue(value) {
+    if (!value) {
+      this.toggleAlertShow("请输入用户名", "error");
+      return;
+    }
+    this.toggleHeader();
+  }
+
+  toggleAlertShow(val, type) {
+    const toggleAlert = (message, type = "success") => {
+      this.setState({
+        message: message,
+        type: type,
+        inShow: !this.state.inShow
+      });
+    };
+    toggleAlert(val, type);
+
+    setTimeout(() => toggleAlert(""), 2000);
+  }
+
   render() {
+    const AlertDom = () => {
+      return (
+        <Fragment>
+          {this.state.inShow && (
+            <Alert
+              message={this.state.message}
+              type={this.state.type}
+              showIcon
+            />
+          )}
+        </Fragment>
+      );
+    };
+
+    const SearchDom = () => {
+      return (
+        <Fragment>
+          {!this.state.headerStatus && (
+            <Search
+              className="search-input"
+              size="large"
+              placeholder="请输入Githun用户名"
+              value={this.state.userName}
+              onChange={this.changeSearchValue}
+              onSearch={this.searchValue}
+              enterButton
+            />
+          )}
+        </Fragment>
+      );
+    };
+
+    const AvatarAndNameDom = () => {
+      return (
+        <Fragment>
+          {this.state.headerStatus && (
+            <Fragment>
+              <h1 className="user-name">{this.state.userName}</h1>
+              <Avatar onClick={this.toggleHeader} size="large" icon="user" />
+            </Fragment>
+          )}
+        </Fragment>
+      );
+    };
+
     return (
-      <header
-        className={`search-name ${this.state.headerStatus ? "active" : ""}`}
-      >
-        <Search
-          className={`search-input ${!this.state.headerStatus ? "active" : ""}`}
-          size="large"
-          placeholder="请输入Githun用户名"
-          onSearch={value => {
-            this.toggleHeader();
-            console.log(value);
-          }}
-          enterButton
-        />
-        <div
-          className={`avatar-img ${this.state.headerStatus ? "active" : ""}`}
+      <Fragment>
+        <header
+          className={`search-name ${this.state.headerStatus && "active"}`}
         >
-          <Avatar onClick={this.toggleHeader} size="large" icon="user" />
-        </div>
-      </header>
+          <SearchDom />
+          <AvatarAndNameDom />
+        </header>
+        <AlertDom />
+      </Fragment>
     );
   }
 }
